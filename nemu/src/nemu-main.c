722ff5f00fbaacfd73c3fe741f7c034943dc9234
tracer-ics2024 <tracer@njuics.org>
2025-03-10 16:41:13 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:13 up  3:08, 19 users,  load average: 0.19, 0.43, 0.53

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,6 +20,14 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+/**
+ * @brief 主函数，程序入口。
+ *
+ * @param argc 命令行参数个数,至少包含程序自身名称，当前命令也算一个参数。
+ * @param argv 命令行参数数组
+ *
+ * @return 0 表示程序正常退出，非0 表示程序异常退出
+ */
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
