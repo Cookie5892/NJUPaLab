9c4744252a0ca493c09425fd884d5d3002ea01c0
tracer-ics2024 <tracer@njuics.org>
2025-03-17 10:26:10 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:26:10 up 34 min,  3 users,  load average: 0.40, 0.37, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,9 +89,7 @@ static int cmd_x(char *args)
 
   if (step != NULL && n_args != NULL)
   {
-    int i = atoi(n_args), j = atoi(step);
-    printf("i=%d---%x,j=%d-----%x",i,i,j,j);
-    word_t val = vaddr_read(atoi(n_args), atoi(step));
+    word_t val = vaddr_read(atoi(n_args), strtoul(step,NULL,0));
     printf("内存内容为：" FMT_WORD "\n", val);
     return 0;
   }
