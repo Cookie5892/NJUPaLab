b6a0c7817b3428b927d1facf70f51f8655e06fb2
tracer-ics2024 <tracer@njuics.org>
2025-03-29 17:16:41 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:41 up  4:37,  3 users,  load average: 1.43, 0.98, 0.82

--- nemu/src/isa/loongarch32r/inst.c
+++ nemu/src/isa/loongarch32r/inst.c
@@ -25,18 +25,12 @@
 enum {
   TYPE_2RI12, TYPE_1RI20,
   TYPE_N, // none
-  TYPE_riscvRI12,
 };
 
 #define src1R()  do { *src1 = R(rj); } while (0)
 #define simm12() do { *imm = SEXT(BITS(i, 21, 10), 12); } while (0)
 #define simm20() do { *imm = SEXT(BITS(i, 24, 5), 20) << 12; } while (0)
 
-//riscv32
-//SEXT当处理有限位宽的立即数时（例如 12 位、20 位），如果最高位（符号位）为1，
-//则需要将其扩展为一个完整的机器字长（例如 32 位或 64 位）的负数表示
-#define riscvsrc1R()  do { *src1 = R(rj); } while (0)
-#define riscvsimm12() do  { *imm = SEXT(BITS(i, 31, 20), 12); } while (0)
 
 static void decode_operand(Decode *s, int *rd_, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -50,19 +44,6 @@ static void decode_operand(Decode *s, int *rd_, word_t *src1, word_t *src2, word
   }
 }
 
-static void decode_operand_isa32(Decode *s, int *rd_, word_t *src1, word_t *src2, word_t *imm, int type){
-  uint32_t i = s->isa.inst;
-  int rj = BITS(i, 19, 15);
-  *rd_ = BITS(i, 11, 7);
-  switch (type) {
-    case TYPE_riscvRI12: riscvsimm12(); riscvsrc1R(); break;
-    case TYPE_2RI12: simm12(); src1R(); break;
-    case TYPE_N: break;
-    default: panic("Unsupport type = %d", type);
-  }
-}
-
-
 
 static int decode_exec(Decode *s) {
   s->dnpc = s->snpc;
@@ -71,7 +52,7 @@ static int decode_exec(Decode *s) {
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   int rd = 0; \
   word_t src1 = 0, src2 = 0, imm = 0; \
-  decode_operand_isa32(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+  decode_operand_isa(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
 
@@ -79,8 +60,6 @@ static int decode_exec(Decode *s) {
   INSTPAT("0001110 ????? ????? ????? ????? ?????" , pcaddu12i, 1RI20 , R(rd) = s->pc + imm);
   INSTPAT("0010100010 ???????????? ????? ?????"   , ld.w     , 2RI12 , R(rd) = Mr(src1 + imm, 4));
   INSTPAT("0010100110 ???????????? ????? ?????"   , st.w     , 2RI12 , Mw(src1 + imm, 4, R(rd)));
-  INSTPAT("???????????? ????? ??? ????? 0010011"  , addi     , riscvRI12  , R(rd) = src1 + imm);
-  
 
   INSTPAT("0000 0000 0010 10100 ????? ????? ?????", break    , N     , NEMUTRAP(s->pc, R(4))); // R(4) is $a0
   INSTPAT("????????????????? ????? ????? ?????"   , inv      , N     , INV(s->pc));
@@ -92,6 +71,6 @@ static int decode_exec(Decode *s) {
 }
 
 int isa_exec_once(Decode *s) {
-  s->isa.inst = inst_fetch(&s->snpc, 4); //根据物理地址获取指定长度内存中的指令，返回指令值
+  s->isa.inst = inst_fetch(&s->snpc, 4); 
   return decode_exec(s);
 }
