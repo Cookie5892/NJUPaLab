b13a17b4101086e5e1eafffa387e989056c013f5
tracer-ics2024 <tracer@njuics.org>
2025-04-01 15:04:45 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:45 up 42 min,  2 users,  load average: 0.28, 0.49, 0.53

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,15 +5,30 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t n = 0;
+  while (*s++){
+    n++;
+  }
+  return n;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;            // 保存目标指针起始位置
+    while ((*dst++ = *src++));   // 复制字符，包括'\0'
+    return ret;                   // 返回目标指针
+  
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strncpy(char *dst, const char *src, size_t n)
+{
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dst[i] = src[i];
+  for (; i < n; i++)
+    dst[i] = '\0';
+
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
