f04f8f1ec80d43961c823fae1c0b9623328ae93e
tracer-ics2024 <tracer@njuics.org>
2025-04-07 11:23:24 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:24 up  1:57,  4 users,  load average: 0.41, 0.51, 0.42

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -134,7 +134,7 @@ int isa_exec_once(Decode *s) {
   return decode_exec(s);
 }
 
-#define MAX_iringbuf_log 5
+#define MAX_iringbuf_log 16
 static char iringbuf_log[MAX_iringbuf_log][128] = {0};
 static int i = 0;
 static void iringbuf(Decode *s) {
