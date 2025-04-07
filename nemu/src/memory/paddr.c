8f1b3df20e03e4c45b74d06c796229415938f76a
tracer-ics2024 <tracer@njuics.org>
2025-04-07 17:37:18 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:37:18 up  8:11,  7 users,  load average: 1.55, 0.91, 0.68

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -66,7 +66,11 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) { 
+    pmem_write(addr, len, data); 
+    printf("Memory write: addr = "FMT_PADDR", len = %d, data = 0x%08x", addr, len, data);
+    return; 
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); 
   printf("Memory write: addr = "FMT_PADDR", len = %d, data = 0x%08x", addr, len, data)
   return);
