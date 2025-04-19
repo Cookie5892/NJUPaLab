e02c687d09c8302c9aff2fa30dad43d9e8c6ae01
tracer-ics2024 <tracer@njuics.org>
2025-04-19 18:00:48 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:00:48 up  1:21,  4 users,  load average: 0.77, 0.67, 0.45

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -245,7 +245,7 @@ print_section_headers();
 //ftrace
 char *ftrace(vaddr_t pc){
   for (int i = 0; i < elf_file->symtab_Nmu; i++){
-    if (elf_file->symtab[i].st_info == STT_FUNC ){
+    if (ELF32_ST_TYPE(elf_file->symtab[i].st_info) == STT_FUNC ){
       if (pc >= elf_file->symtab[i].st_value && pc < (elf_file->symtab[i].st_value + elf_file->symtab[i].st_size))
       return &elf_file->shstrtab[elf_file->symtab[i].st_name];//返回字符串的起始地址，
     }
