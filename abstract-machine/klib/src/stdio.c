ac0f6ddb78e14ebaa444c9f0c4f5ad4cd68449ac
tracer-ics2024 <tracer@njuics.org>
2025-04-30 16:01:29 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:29 up 1 day,  7:03,  6 users,  load average: 0.38, 0.62, 0.65

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -70,11 +70,94 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return result;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *dst = out;
+  const char *p = fmt;
+  if (n == 0 ) return 0;
+  size_t remaining = n > 0 ? n - 1 : 0;
+  size_t written = 0; //记录生成的完整字符串的长度，包括被截断的部分
+
+  while (*p != '\0'){
+    if (*p != '%'){
+      if(remaining > 0){
+        *dst++ = *p;
+        remaining--;
+      }
+      written++;
+      p++;
+      continue;
+    }
+
+    p++;
+    switch (*p){
+      case 'd':{
+        int value = va_arg(ap, int);
+        char buffer[32];
+        char *temp = buffer;
+        if (value < 0){
+          if (remaining){
+            *dst++ = '-';
+            remaining--;
+          }
+          written++;
+        }
+
+        do {
+          *temp++ = '0' + (value % 10);
+          value /= 10;
+        }while (value);
+        while (temp != buffer){
+          if (remaining){
+            *dst++ = *--temp;
+            remaining--;
+          }
+          written++;
+        }
+        break;
+      }
+
+      case 's':{
+        const char *str = va_arg(ap, const char *);
+        while (*str != '\0'){
+          if (remaining){
+            *dst++ = *str;
+            remaining--;
+          }
+          written++;
+          str++;
+        }
+        break;
+      }
+
+      default:{
+        if (remaining > 0){
+          *dst++ = '%';
+          remaining--;
+        }
+        written++;
+        if (remaining > 0){
+          *dst++ = *p;
+          remaining--;
+        }
+        written++;
+        break;
+      }
+    }
+    p++;
+
+  }
+  if (n > 0){
+    *dst = '\0';
+  }
+
+  return written;
 }
 
 #endif
