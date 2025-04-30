5e0ab02f9a962d5076760f93b167be88b624fd12
tracer-ics2024 <tracer@njuics.org>
2025-04-30 10:03:06 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:06 up 1 day,  1:05,  6 users,  load average: 0.80, 0.64, 0.51

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,133 +5,127 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  //assert(s != NULL); 
-  size_t count = 0;
-  while (*s++ != '\0'){
-    count++;
+  const char *p = s;
+  size_t str_len = 0;
+  while ( *p != '\0' ){
+    str_len++;
+    p++;
   }
-  return count;
+  return str_len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  char *ret = dst;            // 保存目标指针起始位置
-    while ((*dst++ = *src++));   // 复制字符，包括'\0'
-    return ret;                   // 返回目标指针
-  
+  size_t i;
+  for ( i = 0; src[i] != '\0'; i++){
+    dst[i] = src[i];
+  }
+
+  dst[i] = src[i];
+  return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n)
-{
+char *strncpy(char *dst, const char *src, size_t n) {
   size_t i;
 
-  for (i = 0; i < n && src[i] != '\0'; i++)
+  for ( i = 0; i < n && src[i] != '\0'; i++ ) {
     dst[i] = src[i];
-  for (; i < n; i++)
+  }
+
+  for ( ; i < n; i++){
     dst[i] = '\0';
+  }
 
   return dst;
 }
 
-char *strcat(char *dst, const char *src)
-{
-    //assert(dst != NULL && src != NULL);
-    char *original_dest = dst;
+char *strcat(char *dst, const char *src) {
+  size_t i;
+  char *p = dst;
+  while (*p != '\0'){
+    p++;
+  }
+  for (i = 0; src[i] != '\0'; i++){
+    p[i] = src[i];
+  }
+  p[i] = src[i];
 
-    // 移动dest指针到目标字符串的末尾
-    while (*dst != '\0')
-    {
-      dst++;
-    }
-
-    // 将src字符串的内容复制到dest指针当前位置
-    while ((*dst++ = *src++) != '\0')
-    {
-      ;
-    }
-    return original_dest;
+  return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  //assert(s1 != NULL && s2 !=NULL);
-  while (*s1 && (*s1 == *s2)) {
-    s1++;
-    s2++;
+int strcmp(const char *s1, const char *s2) {  //重点
+  size_t i = 0;
+  while ( s1[i] != '\0' && s1[i] == s2[i] ){
+    i++;
   }
-return *(unsigned char *)s1 - *(unsigned char *)s2;
-  
+  return (unsigned)s1[i] - (unsigned)s2[i];
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  //assert(s1 != NULL && s2 != NULL);
-    while (n-- > 0 && *s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+  size_t i = 0;
+  while (i < n && s1[i] != '\0' && s1[i] == s2[i]){
+    i++;
+  }
+
+  if (i == n) return 0;
+
+  return (unsigned)s1[i] - (unsigned)s2[i];
 }
 
 void *memset(void *s, int c, size_t n) {
-  //assert(s != NULL);
-    unsigned char* pDest = (unsigned char*)s;
-    c = (unsigned char)c;  // 确保c是一个无符号字符
-    while (n-- > 0) {
-        *pDest++ = c;
-    }
-    return s;
+  unsigned char *p = (unsigned char *)s;
+  while (n--){
+    *p++ = (unsigned char)c;
+  }
+
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  //assert(dst != NULL && src != NULL);
-    char* d = (char*)dst;
-    const char* s = (const char*)src;
-
-    // 如果源地址和目标地址相同，则直接返回目标地址
-    if (d == s) {
-        return dst;
-    }
-
-    // 判断源地址和目标地址的相对位置
-    if (d < s) {
-        // 从前向后拷贝（源地址在目标地址之后或没有重叠）
-        while (n--) {
-            *d++ = *s++;
-        }
-    } else {
-        // 从后向前拷贝（源地址在目标地址之前，有重叠）
-        d += n;
-        s += n;
-        while (n--) {
-            *--d = *--s;
-        }
-    }
+  unsigned char *d = (unsigned char *)dst;
+  const unsigned char *s = (unsigned char *)src;
+  
+  if (d == s) {
     return dst;
+  }
+
+  if ( d < s || d > s + n){
+    while (n--){
+      *d++ = *s++; 
+    }
+  }else {
+    d = d + n;
+    s = s + n;
+    while (n--){
+      *(d--) = *(s--);
+    }
+  }
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-    //assert(out != NULL && in != NULL);
-    char* d = (char*)out;
-    const char* s = (const char*)in;
+  unsigned char *dst = (unsigned char *)out;
+  const unsigned char *src  = (unsigned char *)in;
 
-    while (n--) {
-        *d++ = *s++;
-    }
-    return out;
+  while (n--){
+    *dst++ = *src++;
+  }
+
+  return out;
 }
-
 int memcmp(const void *s1, const void *s2, size_t n) {
-  //assert(s1 != NULL && s2 != NULL);
-  const unsigned char *p1 = (const unsigned char*)s1;
-  const unsigned char *p2 = (const unsigned char*)s2;
-  while (n--)
-  {
-    if (*p1 !=  *p2){
+  const unsigned char *p1 = (const unsigned char *)s1;
+  const unsigned char *p2 = (const unsigned char *)s2; 
+  
+  while (n--){
+    if (*p1 != *p2){
       return (int)(*p1 - *p2);
     }
     p1++;
     p2++;
   }
-  return 0;
 
+  return 0;
 }
 
 #endif
