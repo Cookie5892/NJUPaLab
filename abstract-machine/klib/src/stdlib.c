0ac1f1e398fbfbfb7a968526393861534891159f
tracer-ics2024 <tracer@njuics.org>
2025-05-07 11:30:54 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:30:54 up  2:35,  5 users,  load average: 0.38, 0.38, 0.44

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,10 +33,37 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  
+
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
-  return NULL;
+
+  // 静态变量 addr 用于记录当前堆的分配位置
+  static uintptr_t addr = 0;
+
+  // 如果 addr 尚未初始化，则将其设置为堆的起始地址
+  if (addr == 0) {
+    addr = (uintptr_t)heap.start;
+  }
+
+  // 确保返回的地址满足对齐要求（假设 8 字节对齐）
+  #define ALIGNMENT 8
+  size = (size + ALIGNMENT - 1) & ~(ALIGNMENT - 1); // 向上对齐到 ALIGNMENT 的倍数
+
+  // 检查是否有足够的堆空间
+  if (addr + size > (uintptr_t)heap.end) {
+    return NULL; // 堆空间不足，返回 NULL
+  }
+
+  // 保存当前分配的起始地址
+  uintptr_t aligned = (addr + ALIGNMENT - 1) & ~(ALIGNMENT - 1);
+  void *allocated = (void *)aligned;
+
+  // 更新 addr，指向下一段可用空间
+  addr = aligned + size;
+
+  return allocated;
 }
 
 void free(void *ptr) {
