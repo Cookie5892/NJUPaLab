a2c5aa65bbf7c847994971340cf7fe2feb19f653
tracer-ics2024 <tracer@njuics.org>
2025-05-09 10:34:34 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:34 up  1:02,  4 users,  load average: 0.24, 0.23, 0.36

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE, printf("device read : name = %s, addr = "FMT_PADDR", len = %d, data = 0x%08x\n", map->name, addr, len, ret));
   return ret;
 }
 
