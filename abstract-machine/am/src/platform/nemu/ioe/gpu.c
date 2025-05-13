6b49b2361792d7f682bdec6710a3710a2baa9ef6
tracer-ics2024 <tracer@njuics.org>
2025-05-13 09:31:04 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:31:04 up 41 min,  4 users,  load average: 0.84, 0.47, 0.33

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,7 +17,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint16_t vga_height = vga_d >> 16;
   uint16_t vga_width = vga_d;
   *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = true,
+    .present = true, .has_accel = false,
     .width = vga_width, .height = vga_height,
     .vmemsz = 0 
   };
