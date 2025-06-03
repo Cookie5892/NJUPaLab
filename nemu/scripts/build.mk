af0c4ad46fb997db6297742532358d96473a63ce
tracer-ics2024 <tracer@njuics.org>
2025-06-03 21:24:45 +0800

    >  compile NEMU 2107133052 张梦龙 Linux wp 6.8.0-60-generic #63~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 22 19:00:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:45 up  1:15,  3 users,  load average: 0.69, 1.02, 1.16

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -63,7 +63,7 @@ preprocess-file:
         exit 1; \
     fi
 	@mkdir -p $(OBJ_DIR)
-	$(CC) -O2 -E -P -C $(INCLUDES) $(SRC) -o $(OBJ_DIR)/$(notdir $(SRC:.c=.i)) 
+	$(CC) -O2 -E -P $(INCLUDES) $(SRC) -o $(OBJ_DIR)/$(notdir $(SRC:.c=.i)) 
 	@echo "Preprocessed file generated: $(OBJ_DIR)/$(notdir($(SRC:.c=.i)))"
 
 app: $(BINARY)
